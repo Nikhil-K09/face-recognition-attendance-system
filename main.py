90ad3bcb1127416948ded2cf65b3ab7c166e5d89
Nikhil-K09 <kalavaguntanikhil@gmail.com>
2025-07-11 23:52:51 +0530

    detecting face

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import cv2
+import matplotlib.pyplot as plt
 
-image_path='input/input1.png'
+image_path='input/input2.jpeg'
 img=cv2.imread(image_path)
 
-# Dimensions -- shape
-print(img.shape)
+grayimg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+classifier=cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+
+face=classifier.detectMultiScale(grayimg,scaleFactor=1.1,minSize=(40,40),minNeighbors=5)
+
+for(x,y,w,h) in face:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),5)
+
+img_rgb=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+
+
+plt.figure(figsize=(20,10))
+plt.axis('off')
+plt.imshow(img_rgb)
+plt.show()
